e1d7c5dcae99897c63c72fb261a0bd6394267f0d
JEFFERSON FELIPE ALVES DA SILVA <a9261848@rn.senac.br>
2019-08-14 21:21:04 -0300

    Preparação de tela

--- src/rn/senac/rn/agenda/view/ContatoView.java
+++ src/rn/senac/rn/agenda/view/ContatoView.java
@@ -1,4 +1,84 @@
 package rn.senac.rn.agenda.view;
 
+
+import rn.senac.rn.agenda.model.Contato;
+import rn.senac.rn.agenda.repository.ContatoRepository;
+import java.util.Scanner;
+
 public class ContatoView {
+
+
+    public static Scanner entrada = new Scanner(System.in);
+    private static ContatoRepository repository = new ContatoRepository();
+
+
+    public static void main(String[] args) {
+
+        Integer escolha = -1;
+
+        do  {
+            ContatoView.getMenu();
+            escolha = entrada.nextInt();
+
+            switch (escolha){
+                case 1:
+                    ContatoView.cadastrar();
+                    break;
+                case 2:
+                    ContatoView.listarTodos();
+                    break;
+                case 3:
+                    ContatoView.buscarPeloNome();
+                    break;
+            }
+
+        } while (escolha > 5 || escolha < 0);
+
+    }
+
+    private static void buscarPeloNome() {
+        System.out.println("Digite o nome: ");
+        String nome = entrada.next();
+            repository.buscaPeloNome(nome);
+
+        String textoSaida = "* " + id + " | " + nome + " | " + fone + " *";
+            System.out.println(textoSaida);
+
+    }
+
+    private static void cadastrar() {
+        System.out.println("Digite o nome: ");
+        String nome = entrada.next();
+        System.out.println("Digite o fone: ");
+        String fone = entrada.next();
+        Contato contato = new Contato(nome, fone);
+        repository.salva(contato);
+    }
+
+    private static void listarTodos() {
+
+        for (Contato contato : repository.buscaTodos()){
+            String id = contato.getId().toString();
+            String nome = contato.getNome();
+            String fone = contato.getFone();
+            String textoSaida = "* " + id + " | " + nome + " | " + fone + " *";
+            System.out.println(textoSaida);
+        }
+    }
+
+    private static void getMenu() {
+        String menu = "";
+        menu += "============================================== \n";
+        menu += "1 - Cadastrar Novo Contato                     \n";
+        menu += "2 - Listar Todos os Contatos                   \n";
+        menu += "3 - Buscar Contato  pelo Nome                  \n";
+        menu += "4 - Editar um Contato                          \n";
+        menu += "5 - Excluir um Contato                         \n";
+        menu += "0 - SAIR                                       \n";
+        menu += "============================================== \n";
+        menu += "Opção: ";
+        System.out.print(menu);
+    }
+
+
 }
