b21ddb5bc3583b28449ca5cc6ec7b14f4144bd5e
JEFFERSON FELIPE ALVES DA SILVA <a9261848@rn.senac.br>
2019-10-10 21:25:09 -0300

    deploy

---
+++ src/main/java/br/senac/rn/agenda/config/AgendaApplicationConfig.java
@@ -0,0 +1,21 @@
+package br.senac.rn.agenda.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+
+public class AgendaApplicationConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("https://a9261814-agenda-front.herokuapp.com/")
+                .allowedMethods("GET", "POST");
+    }
+
+
+
+}
